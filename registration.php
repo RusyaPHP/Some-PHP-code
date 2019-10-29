e6661b703b2124b192210e41ffb0c4271ecc5672
RusyaPHP <57153745+RusyaPHP@users.noreply.github.com>
2019-10-29 23:15:27 +0300

    Create  registration.php

---
+++ registration.php
@@ -0,0 +1,34 @@
+require "rb.php";
+R::setup( 'mysql:host=localhost;dbname=dbname',
+     'login', 'password' );
+
+$data = $_POST;
+$errors = array();
+
+if (isset($data['signup'])) { // registration
+
+    if (trim($data['login']) == '') {
+        $errors[] = 'Enter login!';
+    }
+    if ($data['password'] == '') {
+        $errors[] = 'Enter password!';
+    }
+    if (R::count('users', "login = ?", array(
+        $data['login']
+    )) > 0) {
+        $errors[] = 'A user with this login already exists.';
+    }
+    if ($data['g-recaptcha-response'] == 0) {
+        $errors[] = 'Solve the captcha!';
+    }
+    if (empty($errors)) {
+        $user           = R::dispense('users');
+        $user->login    = $data['login'];
+        $user->password = $data['password'];
+        $user->balance  = 0;
+        R::store($user);
+        exit('You are successfully registered!');
+    } else {
+        exit(array_shift($errors));
+    }
+}
